651c0835415163c8a974618e62fa3cba4cca4607
G1 <g1.tommy.jeon@gmail.com>
2021-12-31 16:01:48 +0900

    add: Initialized Spring Project (#1)

---
+++ src/test/java/me/g1tommy/etfportfolioanalyzer/EtfPortfolioAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.g1tommy.etfportfolioanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtfPortfolioAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
